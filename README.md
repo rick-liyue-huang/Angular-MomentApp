a3ebc89fc1a0d76207f9192e395a8eea14638b94
rickliyuehuang <rick.liyue.huang@gmail.com>
2017-06-20 12:54:06 +1000

    produce the distribution edition

--- README.md
+++ README.md
@@ -11,8 +11,10 @@ This is a realtime blog SPA project, I get the data from 'douban' 'moment' serve
 - views/: put some samll html template,
 - scripts/: about the angular service logic coding
 - api/: the server coding in php files 
+- release/: get the distribution edition through gulp.js 
 
 
 ### Notice
 
 In order to get data from 'api/' folder, I put the whole project 'Angular-MomentApp' folder in the 'Applications/MAMP/htdocs/', and open in address of 'localhost/Angular-MomentApp/#/today'. By the way, We also can add the plugin of "no referer image" to explore the pictures in chrome browsers.
+If we use the distribution edition, we just use address of 'localhost/Angular-MomentApp/release/#/today'
--- gulpfile.js
+++ gulpfile.js
@@ -8,39 +8,98 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var htmlmin = require('gulp-htmlmin');
 var autoprefixer = require('gulp-autoprefixer');
+var rev = require('gulp-rev');
+var revcollector = require('gulp-rev-collector');
+var useref = require('gulp-useref');
+var gulpif = require('gulp-if');
+var rename = require('gulp-rename');
 
-gulp.task('less', function() {
 
-	gulp.src('./public/less/*.less')
-		.pipe(less())
-		.pipe(cssmin())
-		.pipe(autoprefixer())
-		.pipe(gulp.dest('./release/public/less'));	
+// deal with .less file
+gulp.task('css', function() {
+
+	return gulp.src('./public/less/main.less')
+		.pipe(less()) // transfer to .css file
+		.pipe(cssmin()) // compass the .css file
+		.pipe(autoprefixer()) // add the prefix on .css
+		.pipe(rev()) // change the .css name with md5
+		.pipe(gulp.dest('./release/public/css')) // save to the new directory
+		.pipe(rev.manifest()) // create the json dictionary
+		.pipe(rename('css-manifest.json'))
+		.pipe(gulp.dest('./release/rev')); // save the dictionary
 });
 
+
+// deal with pictures
 gulp.task('image', function() {
-
-	gulp.src('./public/images/**/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('./release/public/images'));	
-});
-
-gulp.task('js', function() {
 	
-	gulp.src('./scripts/*.js')
-		.pipe(concat('main.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('./release/scripts'));
+	return gulp.src(['./public/images/**/*', './uploads/**/*'], {base: './'})
+		.pipe(imagemin())
+		.pipe(rev())
+		.pipe(gulp.dest('./release'))
+		.pipe(rev.manifest())
+		.pipe(rename('image-manifest.json'))
+		.pipe(gulp.dest('./release/rev'));
 });
 
 
-// keep the directory as original one
-gulp.task('html', function() {
+// deal with .js files
+gulp.task('useref', function() {
 
-	gulp.src(['./index.html', './views/*.html'], {base: './'})
-		.pipe(htmlmin({collapseWhitespace: true, removeComments: true, minifyJS: true, minifyCSS: true}))
+	return gulp.src('./index.html')
+		.pipe(useref())
+		.pipe(gulpif('*.js', uglify()))
+		.pipe(gulpif('*.js', rev()))
+		.pipe(gulp.dest('./release'))
+		.pipe(rev.manifest())
+		.pipe(rename('js-manifest.json'))
+		.pipe(gulp.dest('./release/rev'));
+});
+
+// deal with other files
+gulp.task('other', function() {
+
+	return gulp.src(['./api/*', './public/fonts/*', './public/libs/*', './views/*.html', './favicon.ico'], {base: './'})
+		.pipe(gulp.dest('./release'));
+});
+
+gulp.task('replace', ['css', 'image', 'useref'], function() {
+
+	return gulp.src(['./release/rev/*.json', './release/index.html'])
+		.pipe(revcollector())
 		.pipe(gulp.dest('./release'))
 });
+
+// default
+gulp.task('default', ['replace', 'other']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
